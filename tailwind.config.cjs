17ce80e65086c0b1d791b60f0a4a1f153d8f7945
Joshua Oyewole <joshpp2013@gmail.com>
2022-12-08 11:42:34 +0100

    change 2xl screen to be from 1800px

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       'md': '769px',
       'lg': '1024px',
       'xl': '1280px',
-      '2xl': '1536px',
+      '2xl': '1800px',
     },
     colors: {
       'blue': '#1A73CE',
