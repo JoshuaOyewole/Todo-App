029f387c220b9b2505105d15faad7f2171631ea7
Joshua Oyewole <joshpp2013@gmail.com>
2022-12-09 04:30:16 +0100

    added env variable to firebase config file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPqX7ZlFUfpGP4VikI5zJrWN7wGCkukAA",
+  apiKey: import.meta.env.apiKey,
   authDomain: "todoapp-assessment.firebaseapp.com",
   projectId: "todoapp-assessment",
-  storageBucket: "todoapp-assessment.appspot.com",
-  messagingSenderId: "1060916988696",
-  appId: "1:1060916988696:web:aa3db8cbd33616b82aac6d",
-  measurementId: "G-L8P6BYHHNM"
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
+  measurementId: import.meta.env.measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {db};
